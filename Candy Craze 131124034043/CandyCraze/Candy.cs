902d846cf342f038140744edcefdb605a61f53c6
Kumar <jitendra.i.kumar@capgemini.com>
2024-11-27 11:16:20 +0530

    Added:Candy Craze Solutions

---
+++ Candy Craze 131124034043/CandyCraze/Candy.cs
@@ -0,0 +1,7 @@
+﻿namespace CandyCraze //DO NOT change the namespace name
+{
+    public class Candy //DO NOT change the class name
+    {
+        //Implement your code here
+    }
+}
---
+++ Candy Craze 131124034043/CandyCraze/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CandyCraze //DO NOT change the namespace name
+{
+    public class Program //DO NOT change the class name
+    {
+        //Implement the methods here
+
+        public static void Main(string[] args)  //DO NOT change the method signature
+        {
+            //Implement your code here
+        }
+    }
+}
